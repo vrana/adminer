0970a583659fb3b62b382c24f4b69fbb6a5c4d41
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2007-07-05 07:34:42 +0000

    Prepare for single language version
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@32 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- _compile.php
+++ _compile.php
@@ -1,6 +1,10 @@
 <?php
+function remove_lang($match) {
+	global $LANG;
+	return lang(strtr($match[2], array("\\'" => "'", "\\\\" => "\\")));
+}
+
 function put_file($match) {
-	//! exit on error with require, _once
 	$return = file_get_contents($match[4]);
 	$return = preg_replace("~\\?>?\n?\$~", '', $return);
 	if (substr_count($return, "<?php") - substr_count($return, "?>") <= 0 && !$match[5]) {
@@ -13,8 +17,17 @@ function put_file($match) {
 	return $return;
 }
 
+error_reporting(E_ALL & ~E_NOTICE);
 $file = file_get_contents("index.php");
+$LANG = (strlen($_SERVER["argv"][1]) == 2 ? $_SERVER["argv"][1] : "");
+if ($LANG) {
+	$file = str_replace("include \"./lang.inc.php\";\n", "", $file);
+}
 $file = preg_replace_callback('~(<\\?php\\s*)?(include|require)(_once)? "([^"]*)";(\\s*\\?>)?~', 'put_file', $file);
+if ($LANG) {
+	include "./lang.inc.php";
+	preg_replace_callback("~(<\\?php\\s*echo )?lang\\('((?:[^\\\\']*|\\\\.)+)'\\)(;\\s*\\?>)?~s", 'remove_lang', $file);
+}
 //! remove spaces and comments
-file_put_contents("phpMinAdmin.php", $file);
+file_put_contents("phpMinAdmin" . ($LANG ? "-$LANG" : "") . ".php", $file);
 echo "phpMinAdmin.php created.\n";
