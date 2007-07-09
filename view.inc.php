c185b6e477826e9890303f536b5bd51398a89059
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2007-07-09 22:23:11 +0000

    Hide create modifiers
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@91 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- view.inc.php
+++ view.inc.php
@@ -1,3 +1,3 @@
 <?php
 page_header(lang('View') . ": " . htmlspecialchars($_GET["view"]));
-echo "<pre>" . htmlspecialchars(mysql_result(mysql_query("SHOW CREATE VIEW " . idf_escape($_GET["view"])), 0, 1)) . "</pre>\n";
+echo "<pre>" . htmlspecialchars(preg_replace('~^.* AS ~U', '', mysql_result(mysql_query("SHOW CREATE VIEW " . idf_escape($_GET["view"])), 0, 1))) . "</pre>\n";
