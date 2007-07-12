50b85f04eb634dcf17ec8ea372a88441cb76661f
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2007-07-12 10:51:49 +0000

    Clear database cache on homepage
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@154 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- connect.inc.php
+++ connect.inc.php
@@ -1,5 +1,6 @@
 <?php
 if (!(strlen($_GET["db"]) ? $mysql->select_db($_GET["db"]) : isset($_GET["sql"]) || isset($_GET["dump"]) || isset($_GET["database"]))) {
+	unset($_SESSION["databases"]);
 	page_header(lang('Select database'));
 	if (strlen($_GET["db"])) {
 		echo "<p class='error'>" . lang('Invalid database.') . "</p>\n";
