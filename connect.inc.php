49948b3a53e4ba95d8228b803bd97f14900c35e2
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2008-03-18 12:31:55 +0000

    Logged as
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@376 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- connect.inc.php
+++ connect.inc.php
@@ -12,6 +12,7 @@ if (!(strlen($_GET["db"]) ? $mysql->select_db($_GET["db"]) : isset($_GET["sql"])
 		echo '<p><a href="' . htmlspecialchars($SELF) . 'privileges=">' . lang('Privileges') . "</a></p>\n";
 		echo '<p><a href="' . htmlspecialchars($SELF) . 'processlist=">' . lang('Process list') . "</a></p>\n";
 		echo "<p>" . lang('MySQL version: %s through PHP extension %s', "<b>$mysql->server_info</b>", "<b>$mysql->extension</b>") . "</p>\n";
+		echo "<p>" . lang('Logged as: %s', "<b>" . htmlspecialchars($mysql->result($mysql->query("SELECT USER()"))) . "</b>") . "</p>\n";
 	}
 	page_footer("db");
 	exit;
