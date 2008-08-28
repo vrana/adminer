f390b1af14403c6d472c0fad20e0f499dd0d10dc
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2008-08-28 12:35:13 +0000

    Don't wrap selected lines
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@463 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- select.inc.php
+++ select.inc.php
@@ -238,7 +238,7 @@ for (var i=0; <?php echo $i; ?> > i; i++) {
 					echo "</tr></thead>\n";
 				}
 				$unique_idf = implode('&amp;', unique_idf($row, $indexes));
-				echo '<tr>' . (count($select) == count($group) ? '<td><input type="checkbox" name="delete[]" value="' . $unique_idf . '" /> <a href="' . htmlspecialchars($SELF) . 'edit=' . urlencode($_GET['select']) . '&amp;' . $unique_idf . '">' . lang('edit') . '</a></td>' : '');
+				echo '<tr style="white-space: nowrap;">' . (count($select) == count($group) ? '<td><input type="checkbox" name="delete[]" value="' . $unique_idf . '" /> <a href="' . htmlspecialchars($SELF) . 'edit=' . urlencode($_GET['select']) . '&amp;' . $unique_idf . '">' . lang('edit') . '</a></td>' : '');
 				foreach ($row as $key => $val) {
 					if (!isset($val)) {
 						$val = "<i>NULL</i>";
