5e7f1991d923ace29a3a9260294635680eeb771b
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2009-06-15 10:57:43 +0000

    Display export in IE
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@697 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- adminer/include/export.inc.php
+++ adminer/include/export.inc.php
@@ -169,7 +169,9 @@ function dump_headers($identifier, $multi_table = false) {
 	$filename = (strlen($identifier) ? friendly_url($identifier) : "dump");
 	$ext = ($_POST["format"] == "sql" ? "sql" : ($multi_table ? "tar" : "csv"));
 	header("Content-Type: " . ($ext == "tar" ? "application/x-tar" : ($ext == "sql" || $_POST["output"] != "file" ? "text/plain" : "text/csv")) . "; charset=utf-8");
-	header("Content-Disposition: " . ($_POST["output"] == "file" ? "attachment" : "inline") . "; filename=$filename.$ext");
+	if ($_POST["output"] == "file") {
+		header("Content-Disposition: attachment; filename=$filename.$ext");
+	}
 	return $ext;
 }
 
