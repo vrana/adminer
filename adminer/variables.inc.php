1205bad5269f7d25c0be52a30ffa9034e46e67d3
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2009-06-16 17:05:25 +0000

    Change JUSH identifier
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@713 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- adminer/variables.inc.php
+++ adminer/variables.inc.php
@@ -5,7 +5,7 @@ echo "<table cellspacing='0'>\n";
 $result = $dbh->query("SHOW VARIABLES");
 while ($row = $result->fetch_assoc()) {
 	echo "<tr>";
-	echo "<th><code class='jush-sql_set'>" . htmlspecialchars($row["Variable_name"]) . "</code></th>";
+	echo "<th><code class='jush-sqlset'>" . htmlspecialchars($row["Variable_name"]) . "</code></th>";
 	echo "<td>" . (strlen(trim($row["Value"])) ? htmlspecialchars($row["Value"]) : "&nbsp;") . "</td>";
 	echo "</tr>\n";
 }
