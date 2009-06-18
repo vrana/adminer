481e838ba21d72539a62168d12d55ba450592a9d
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2009-06-18 04:27:02 +0000

    Simplify includes
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@715 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- adminer/download.inc.php
+++ adminer/download.inc.php
@@ -2,3 +2,4 @@
 header("Content-Type: application/octet-stream");
 header("Content-Disposition: attachment; filename=" . friendly_url("$_GET[download]-" . implode("_", $_GET["where"])) . "." . friendly_url($_GET["field"]));
 echo $dbh->result($dbh->query("SELECT " . idf_escape($_GET["field"]) . " FROM " . idf_escape($_GET["download"]) . " WHERE " . implode(" AND ", where($_GET)) . " LIMIT 1"));
+exit; // don't output footer
--- adminer/index.php
+++ adminer/index.php
@@ -58,79 +58,77 @@ include "./include/connect.inc.php";
 include "./include/editing.inc.php";
 include "./include/export.inc.php";
 
+$on_actions = array("RESTRICT", "CASCADE", "SET NULL", "NO ACTION");
+$enum_length = '\'(?:\'\'|[^\'\\\\]+|\\\\.)*\'|"(?:""|[^"\\\\]+|\\\\.)*"';
+$inout = array("IN", "OUT", "INOUT");
+$confirm = " onclick=\"return confirm('" . lang('Are you sure?') . "');\"";
+$error = "";
+
 if (isset($_GET["download"])) {
 	include "./download.inc.php";
-} else { // outputs footer
-	$on_actions = array("RESTRICT", "CASCADE", "SET NULL", "NO ACTION");
-	$enum_length = '\'(?:\'\'|[^\'\\\\]+|\\\\.)*\'|"(?:""|[^"\\\\]+|\\\\.)*"';
-	$inout = array("IN", "OUT", "INOUT");
-	$confirm = " onclick=\"return confirm('" . lang('Are you sure?') . "');\"";
-	
-	$error = "";
-	if (isset($_GET["table"])) {
-		include "./table.inc.php";
-	} elseif (isset($_GET["view"])) {
-		include "./view.inc.php";
-	} elseif (isset($_GET["schema"])) {
-		include "./schema.inc.php";
-	} elseif (isset($_GET["dump"])) {
-		include "./dump.inc.php";
-	} elseif (isset($_GET["privileges"])) {
-		include "./privileges.inc.php";
-	} else { // uses CSRF token
-		$token = $_SESSION["tokens"][$_GET["server"]];
-		if ($_POST) {
-			if ($_POST["token"] != $token) {
-				$error = lang('Invalid CSRF token. Send the form again.');
-			}
-		} elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
-			$error = lang('Too big POST data. Reduce the data or increase the "post_max_size" configuration directive.');
+} elseif (isset($_GET["table"])) {
+	include "./table.inc.php";
+} elseif (isset($_GET["view"])) {
+	include "./view.inc.php";
+} elseif (isset($_GET["schema"])) {
+	include "./schema.inc.php";
+} elseif (isset($_GET["dump"])) {
+	include "./dump.inc.php";
+} elseif (isset($_GET["privileges"])) {
+	include "./privileges.inc.php";
+} else { // uses CSRF token
+	$token = $_SESSION["tokens"][$_GET["server"]];
+	if ($_POST) {
+		if ($_POST["token"] != $token) {
+			$error = lang('Invalid CSRF token. Send the form again.');
 		}
-		if (isset($_GET["default"])) {
-			$_GET["edit"] = $_GET["default"];
-		}
-		if (isset($_GET["select"]) && $_POST && (!$_POST["delete"] && !$_POST["export"] && !$_POST["import"] && !$_POST["save"])) {
-			$_GET["edit"] = $_GET["select"];
-		}
-		if (isset($_GET["callf"])) {
-			$_GET["call"] = $_GET["callf"];
-		}
-		if (isset($_GET["function"])) {
-			$_GET["procedure"] = $_GET["function"];
-		}
-		if (isset($_GET["sql"])) {
-			include "./sql.inc.php";
-		} elseif (isset($_GET["edit"])) {
-			include "./edit.inc.php";
-		} elseif (isset($_GET["create"])) {
-			include "./create.inc.php";
-		} elseif (isset($_GET["indexes"])) {
-			include "./indexes.inc.php";
-		} elseif (isset($_GET["database"])) {
-			include "./database.inc.php";
-		} elseif (isset($_GET["call"])) {
-			include "./call.inc.php";
-		} elseif (isset($_GET["foreign"])) {
-			include "./foreign.inc.php";
-		} elseif (isset($_GET["createv"])) {
-			include "./createv.inc.php";
-		} elseif (isset($_GET["event"])) {
-			include "./event.inc.php";
-		} elseif (isset($_GET["procedure"])) {
-			include "./procedure.inc.php";
-		} elseif (isset($_GET["trigger"])) {
-			include "./trigger.inc.php";
-		} elseif (isset($_GET["user"])) {
-			include "./user.inc.php";
-		} elseif (isset($_GET["processlist"])) {
-			include "./processlist.inc.php";
-		} elseif (isset($_GET["select"])) {
-			include "./select.inc.php";
-		} elseif (isset($_GET["variables"])) {
-			include "./variables.inc.php";
-		} else {
-			include "./db.inc.php";
-		}
+	} elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$error = lang('Too big POST data. Reduce the data or increase the "post_max_size" configuration directive.');
 	}
-	page_footer();
+	if (isset($_GET["default"])) {
+		$_GET["edit"] = $_GET["default"];
+	}
+	if (isset($_GET["select"]) && $_POST && (!$_POST["delete"] && !$_POST["export"] && !$_POST["import"] && !$_POST["save"])) {
+		$_GET["edit"] = $_GET["select"];
+	}
+	if (isset($_GET["callf"])) {
+		$_GET["call"] = $_GET["callf"];
+	}
+	if (isset($_GET["function"])) {
+		$_GET["procedure"] = $_GET["function"];
+	}
+	if (isset($_GET["sql"])) {
+		include "./sql.inc.php";
+	} elseif (isset($_GET["edit"])) {
+		include "./edit.inc.php";
+	} elseif (isset($_GET["create"])) {
+		include "./create.inc.php";
+	} elseif (isset($_GET["indexes"])) {
+		include "./indexes.inc.php";
+	} elseif (isset($_GET["database"])) {
+		include "./database.inc.php";
+	} elseif (isset($_GET["call"])) {
+		include "./call.inc.php";
+	} elseif (isset($_GET["foreign"])) {
+		include "./foreign.inc.php";
+	} elseif (isset($_GET["createv"])) {
+		include "./createv.inc.php";
+	} elseif (isset($_GET["event"])) {
+		include "./event.inc.php";
+	} elseif (isset($_GET["procedure"])) {
+		include "./procedure.inc.php";
+	} elseif (isset($_GET["trigger"])) {
+		include "./trigger.inc.php";
+	} elseif (isset($_GET["user"])) {
+		include "./user.inc.php";
+	} elseif (isset($_GET["processlist"])) {
+		include "./processlist.inc.php";
+	} elseif (isset($_GET["select"])) {
+		include "./select.inc.php";
+	} elseif (isset($_GET["variables"])) {
+		include "./variables.inc.php";
+	} else {
+		include "./db.inc.php";
+	}
 }
+page_footer();
