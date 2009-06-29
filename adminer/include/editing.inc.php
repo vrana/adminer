a889c1748d5b37da01ee8fd535ee3bd95b258cd2
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2009-06-29 10:25:04 +0000

    Remove unused separator
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@765 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- adminer/include/editing.inc.php
+++ adminer/include/editing.inc.php
@@ -1,10 +1,10 @@
 <?php
-function input($name, $field, $value, $separator = "</td><td>") { //! pass empty separator if there are no functions in the whole table
+function input($name, $field, $value) {
 	global $types;
 	$name = htmlspecialchars(bracket_escape($name));
-	echo "<td" . ($separator ? " class='function'" : "") . ">";
+	echo "<td class='function'>";
 	if ($field["type"] == "enum") {
-		echo ($separator ? "&nbsp;$separator" : "") . (isset($_GET["select"]) ? ' <label><input type="radio" name="fields[' . $name . ']" value="-1" checked="checked" /><em>' . lang('original') . '</em></label>' : "");
+		echo "&nbsp;</td><td>" . (isset($_GET["select"]) ? ' <label><input type="radio" name="fields[' . $name . ']" value="-1" checked="checked" /><em>' . lang('original') . '</em></label>' : "");
 		if ($field["null"] || isset($_GET["default"])) {
 			echo ' <label><input type="radio" name="fields[' . $name . ']" value=""' . (($field["null"] ? isset($value) : strlen($value)) || isset($_GET["select"]) ? '' : ' checked="checked"') . ' />' . ($field["null"] ? '<em>NULL</em>' : '') . '</label>';
 		}
@@ -43,7 +43,7 @@ function input($name, $field, $value, $separator = "</td><td>") { //! pass empty
 		if ($field["null"] || isset($_GET["default"])) {
 			array_unshift($options, "NULL");
 		}
-		echo (count($options) > 1 || isset($_GET["select"]) ? '<select name="function[' . $name . ']">' . (isset($_GET["select"]) ? '<option value="orig">' . lang('original') . '</option>' : '') . optionlist($options, (isset($value) ? (string) $_POST["function"][$name] : null)) . '</select>' : ($separator ? "&nbsp;" : "")) . $separator;
+		echo (count($options) > 1 || isset($_GET["select"]) ? '<select name="function[' . $name . ']">' . (isset($_GET["select"]) ? '<option value="orig">' . lang('original') . '</option>' : '') . optionlist($options, (isset($value) ? (string) $_POST["function"][$name] : null)) . '</select>' : "&nbsp;") . '</td><td>';
 		if ($field["type"] == "set") { //! 64 bits
 			preg_match_all("~'((?:[^']+|'')*)'~", $field["length"], $matches);
 			foreach ($matches[1] as $i => $val) {
