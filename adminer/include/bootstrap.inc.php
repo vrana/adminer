500a835e9d856d6d72a02483b8639470aed538a5
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2009-09-14 15:12:51 +0000

    PHP 6 compatibility
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@1108 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- adminer/include/bootstrap.inc.php
+++ adminer/include/bootstrap.inc.php
@@ -70,7 +70,9 @@ if (get_magic_quotes_gpc()) {
     }
     unset($process);
 }
-set_magic_quotes_runtime(false);
+if (function_exists("set_magic_quotes_runtime")) {
+	set_magic_quotes_runtime(false);
+}
 @set_time_limit(0); // @ - can be disabled
 
 define("DB", $_GET["db"]); // for the sake of speed and size
