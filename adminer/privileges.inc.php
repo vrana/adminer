342c1dce3bac667606d911effeedd932210bddad
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2009-09-22 10:33:56 +0000

    Remove HTML indent
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@1123 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- adminer/privileges.inc.php
+++ adminer/privileges.inc.php
@@ -4,14 +4,14 @@ page_header(lang('Privileges'));
 $result = $dbh->query("SELECT User, Host FROM mysql.user ORDER BY Host, User");
 if (!$result) {
 	?>
-	<form action=""><p>
-	<?php if (SID) { ?><input type="hidden" name="<?php echo session_name(); ?>" value="<?php echo h(session_id()); ?>"><?php } ?>
-	<?php if (strlen($_GET["server"])) { ?><input type="hidden" name="server" value="<?php echo h($_GET["server"]); ?>"><?php } ?>
-	<?php echo lang('Username'); ?>: <input name="user">
-	<?php echo lang('Server'); ?>: <input name="host" value="localhost">
-	<input type="hidden" name="grant" value="">
-	<input type="submit" value="<?php echo lang('Edit'); ?>">
-	</form>
+<form action=""><p>
+<?php if (SID) { ?><input type="hidden" name="<?php echo session_name(); ?>" value="<?php echo h(session_id()); ?>"><?php } ?>
+<?php if (strlen($_GET["server"])) { ?><input type="hidden" name="server" value="<?php echo h($_GET["server"]); ?>"><?php } ?>
+<?php echo lang('Username'); ?>: <input name="user">
+<?php echo lang('Server'); ?>: <input name="host" value="localhost">
+<input type="hidden" name="grant" value="">
+<input type="submit" value="<?php echo lang('Edit'); ?>">
+</form>
 <?php
 	// list logged user, information_schema.USER_PRIVILEGES lists just the current user too
 	$result = $dbh->query("SELECT SUBSTRING_INDEX(CURRENT_USER, '@', 1) AS User, SUBSTRING_INDEX(CURRENT_USER, '@', -1) AS Host");
