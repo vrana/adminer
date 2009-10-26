ffc103d87791e9c6f4f43698520654c1db999bf3
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2009-10-26 16:13:42 +0000

    Compare floats as strings (bug #2886518)
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@1215 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- adminer/include/functions.inc.php
+++ adminer/include/functions.inc.php
@@ -167,7 +167,7 @@ function where($where) {
 	$return = array();
 	foreach ((array) $where["where"] as $key => $val) {
 		$key = bracket_escape($key, "back");
-		$return[] = (preg_match('~^[A-Z0-9_]+\\(`(?:[^`]|``)+`\\)$~', $key) ? $key : idf_escape($key)) . " = " . exact_value($val); //! enum and set, columns looking like functions
+		$return[] = (preg_match('~^[A-Z0-9_]+\\(`(?:[^`]|``)+`\\)$~', $key) ? $key : idf_escape($key)) . " LIKE " . exact_value(addcslashes($val, "%_")); // LIKE because of floats //! enum and set, columns looking like functions
 	}
 	foreach ((array) $where["null"] as $key) {
 		$key = bracket_escape($key, "back");
