10b7f135a533c294bbe93741db480bdf72358318
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2009-11-03 13:30:42 +0000

    Whitespace
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@1228 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- adminer/include/bootstrap.inc.php
+++ adminer/include/bootstrap.inc.php
@@ -55,19 +55,19 @@ session_start();
 
 // disable magic quotes to be able to use database escaping function
 if (get_magic_quotes_gpc()) {
-    $process = array(&$_GET, &$_POST, &$_COOKIE);
-    while (list($key, $val) = each($process)) {
-        foreach ($val as $k => $v) {
-            unset($process[$key][$k]);
-            if (is_array($v)) {
-                $process[$key][stripslashes($k)] = $v;
-                $process[] = &$process[$key][stripslashes($k)];
-            } else {
-                $process[$key][stripslashes($k)] = ($filter ? $v : stripslashes($v));
-            }
-        }
-    }
-    unset($process);
+	$process = array(&$_GET, &$_POST, &$_COOKIE);
+	while (list($key, $val) = each($process)) {
+		foreach ($val as $k => $v) {
+			unset($process[$key][$k]);
+			if (is_array($v)) {
+				$process[$key][stripslashes($k)] = $v;
+				$process[] = &$process[$key][stripslashes($k)];
+			} else {
+				$process[$key][stripslashes($k)] = ($filter ? $v : stripslashes($v));
+			}
+		}
+	}
+	unset($process);
 }
 if (function_exists("set_magic_quotes_runtime")) {
 	set_magic_quotes_runtime(false);
