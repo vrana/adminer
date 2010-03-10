b667bfbe2da7815e898828f24f1989bb4558f968
jakubvrana <jakubvrana@7c3ca157-0c34-0410-bff1-cbf682f78f5c>
2010-03-10 21:42:48 +0000

    Function is not defined in older versions
    git-svn-id: https://adminer.svn.sourceforge.net/svnroot/adminer/trunk@1341 7c3ca157-0c34-0410-bff1-cbf682f78f5c

--- version.js
+++ version.js
@@ -1,2 +1,6 @@
 // downloaded from repository by verifyVersion()
-cookie('adminer_version=2.3.0', 7); // last released version, valid for 7 days
+(function () { // cookie function is not defined in older versions
+	var date = new Date();
+	date.setDate(date.getDate() + 7); // valid for 7 days
+	document.cookie = 'adminer_version=2.3.0; expires=' + date; // last released version
+})();
