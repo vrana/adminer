d69e8c4b08bf25c1e656d0175085ea0e57ff4752
Jakub Vrana <jakub@vrana.cz>
2010-10-14 22:10:02 +0200

    Directory structure

--- tests/iexplore.bat
+++ tests/iexplore.bat
@@ -1,1 +1,1 @@
-java -jar "C:\Program Files\Selenium\selenium-server.jar" -htmlSuite "*iexplore" http://localhost "%CD%\selenium.html" results.html
+java -jar "C:\Program Files\Selenium\selenium-server.jar" -htmlSuite "*iexplore" http://localhost/adminer/ "%CD%\selenium.html" results.html
