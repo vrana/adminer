4c562fd7cce982a6d48c733ce55c9392bb0a7ef9
Jakub Vrana <jakub@vrana.cz>
2010-10-15 12:49:58 +0200

    Unused file

--- version.js
+++
@@ -1,6 +0,0 @@
-// downloaded from repository by verifyVersion() before Adminer 3.0.0
-(function () { // cookie function is not defined in older versions
-	var date = new Date();
-	date.setDate(date.getDate() + 7); // valid for 7 days
-	document.cookie = 'adminer_version=2.3.2; expires=' + date; // last released version
-})();
