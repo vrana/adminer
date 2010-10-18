b50a0fc61578a79b641c92db2cfc8d9755bb8cdf
Jakub Vrana <jakub@vrana.cz>
2010-10-18 23:09:26 +0200

    Disable zend.ze1_compatibility_mode (bug #3089645)

--- adminer/include/bootstrap.inc.php
+++ adminer/include/bootstrap.inc.php
@@ -43,6 +43,7 @@ if (function_exists("set_magic_quotes_runtime")) {
 	set_magic_quotes_runtime(false);
 }
 @set_time_limit(0); // @ - can be disabled
+@ini_set("zend.ze1_compatibility_mode", false); // @ - deprecated
 
 include "../adminer/include/lang.inc.php";
 include "../adminer/lang/$LANG.inc.php";
