b12756643f961823622caaafd1ccf5a1923f47aa
Jakub Vrana <jakub@vrana.cz>
2010-10-22 14:25:56 +0200

    Comment

--- adminer/include/adminer.inc.php
+++ adminer/include/adminer.inc.php
@@ -36,7 +36,7 @@ class Adminer {
 	* @return null
 	*/
 	function headers() {
-		header("X-Frame-Options: deny"); // ClickJacking protection in IE8, Safari 4, Chrome 2, Firefox NoScript plugin
+		header("X-Frame-Options: deny"); // ClickJacking protection in IE8, Safari 4, Chrome 2, Firefox 3.6.9
 		header("X-XSS-Protection: 0"); // prevents introducing XSS in IE8 by removing safe parts of the page
 	}
 	
