12443f2f4c90efc1fca907a28eb0ceee403f4f79
Jakub Vrana <jakub@vrana.cz>
2011-01-24 15:35:06 +0100

    Send <input type='image'> by AJAX

--- adminer/static/functions.js
+++ adminer/static/functions.js
@@ -396,8 +396,8 @@ function bodyClick(event, db, ns) {
 		var match2 = /&ns=([^&]*)/.exec(el.href);
 		return !(db == (match ? match[1] : '') && ns == (match2 ? match2[1] : '') && ajaxMain(el.href, '', event));
 	}
-	if (/^input$/i.test(el.tagName) && /submit|image/.test(el.type) && el.name != 'logout' && !/&(database|scheme|create|view|sql|user|dump)=/.test(location.href)) {
-		return !ajaxForm(el.form, (el.name ? encodeURIComponent(el.name) + '=1' : ''));
+	if (/^input$/i.test(el.tagName) && (el.type == 'image' || (el.type == 'submit' && el.name != 'logout' && !/&(database|scheme|create|view|sql|user|dump)=/.test(location.href)))) {
+		return !ajaxForm(el.form, (el.name ? encodeURIComponent(el.name) + (el.type == 'image' ? '.x' : '') + '=1' : ''));
 	}
 	return true;
 }
