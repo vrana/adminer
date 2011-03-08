1420b6a25975c1402ba613945965c7575b3573b6
Jakub Vrana <jakub@vrana.cz>
2011-03-08 13:46:37 +0100

    Esc to focus next field in Tab textarea (thanks to David Grudl)

--- adminer/static/functions.js
+++ adminer/static/functions.js
@@ -165,16 +165,26 @@ function selectAddRow(field) {
 * @return boolean
 */
 function textareaKeydown(target, event, tab, button) {
-	if (tab && event.keyCode == 9 && !event.shiftKey && !event.altKey && !event.ctrlKey && !event.metaKey) {
-		// inspired by http://pallieter.org/Projects/insertTab/
-		if (target.setSelectionRange) {
-			var start = target.selectionStart;
-			target.value = target.value.substr(0, start) + '\t' + target.value.substr(target.selectionEnd);
-			target.setSelectionRange(start + 1, start + 1);
-			return false; //! still loses focus in Opera, can be solved by handling onblur
-		} else if (target.createTextRange) {
-			document.selection.createRange().text = '\t';
-			return false;
+	if (tab && !event.shiftKey && !event.altKey && !event.ctrlKey && !event.metaKey) {
+		if (event.keyCode == 9) {
+			// inspired by http://pallieter.org/Projects/insertTab/
+			if (target.setSelectionRange) {
+				var start = target.selectionStart;
+				target.value = target.value.substr(0, start) + '\t' + target.value.substr(target.selectionEnd);
+				target.setSelectionRange(start + 1, start + 1);
+				return false; //! still loses focus in Opera, can be solved by handling onblur
+			} else if (target.createTextRange) {
+				document.selection.createRange().text = '\t';
+				return false;
+			}
+		} else if (event.keyCode == 27) {
+			var els = target.form.elements;
+			for (var i=1; i < els.length; i++) {
+				if (els[i-1] == target) {
+					els[i].focus();
+					break;
+				}
+			}
 		}
 	}
 	if (event.ctrlKey && (event.keyCode == 13 || event.keyCode == 10) && !event.altKey && !event.metaKey) { // shiftKey allowed
