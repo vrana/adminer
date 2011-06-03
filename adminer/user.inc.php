ae50ef7594e2134379f2f109af250e3009661c74
Jakub Vrana <jakub@vrana.cz>
2011-06-03 14:02:19 +0200

    Don't overwrite privileges for existing users

--- adminer/user.inc.php
+++ adminer/user.inc.php
@@ -111,7 +111,7 @@ if ($_POST) {
 	if ($old_pass != "") {
 		$row["hashed"] = true;
 	}
-	$grants[$_GET["db"] != "" ? idf_escape($_GET["db"]) . ".*" : ""] = true;
+	$grants[$_GET["db"] != "" && $_GET["user"] == "" ? idf_escape($_GET["db"]) . ".*" : ""] = array();
 }
 
 ?>
