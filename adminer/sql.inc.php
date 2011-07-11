7fa82636293c90c631471dad3e0c299853f0fe1c
Jakub Vrana <jakub@vrana.cz>
2011-07-11 09:55:47 +0200

    Explain links

--- adminer/sql.inc.php
+++ adminer/sql.inc.php
@@ -122,7 +122,7 @@ if (!$error && $_POST) {
 										$id = "explain-$commands";
 										echo ", <a href='#$id' onclick=\"return !toggle('$id');\">EXPLAIN</a>$export\n";
 										echo "<div id='$id' class='hidden'>\n";
-										select($explain, $connection2, ($jush == "sql" ? "http://dev.mysql.com/doc/refman/" . substr($connection->server_info, 0, 3) . "/en/explain-output.html#" : ""));
+										select($explain, $connection2, ($jush == "sql" ? "http://dev.mysql.com/doc/refman/" . substr($connection->server_info, 0, 3) . "/en/explain-output.html#explain_" : ""));
 										echo "</div>\n";
 									} else {
 										echo "$export\n";
