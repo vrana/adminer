9c6a5cd542bf3b1fa46d97cb6aac3bb64c7c7baf
Jakub Vrana <jakub@vrana.cz>
2011-08-10 18:18:02 +0200

    Comment

--- plugins/sql-log.php
+++ plugins/sql-log.php
@@ -1,6 +1,6 @@
 <?php
 
-/** Log all queries to SQL file
+/** Log all queries to SQL file (manual queries through SQL command are not logged)
 * @author Jakub Vrana, http://www.vrana.cz/
 * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
 * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
