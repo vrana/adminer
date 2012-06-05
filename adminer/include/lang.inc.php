a2378af00815e1d4efca4aeb77096f6987fa37a5
Jakub Vrana <jakub@vrana.cz>
2012-06-04 17:13:55 -0700

    Display Indian languages together

--- adminer/include/lang.inc.php
+++ adminer/include/lang.inc.php
@@ -25,9 +25,9 @@ $langs = array(
 	'zh-tw' => '繁體中文', // http://tzangms.com
 	'ja' => '日本語', // Hitoshi Ozawa - http://sourceforge.jp/projects/oss-ja-jpn/releases/
 	'ta' => 'த‌மிழ்', // G. Sampath Kumar, Chennai, India, sampathkumar11@gmail.com
+	'bn' => 'বাংলা', // Tanmay Sarkar - calcutta, India - tanmay@kisholoy.org
 	'ar' => 'العربية', // Y.M Amine - Algeria - nbr7@live.fr
 	'fa' => 'فارسی', // mojtaba barghbani - Iran - mbarghbani@gmail.com
-	'bn' => 'বাংলা', // Tanmay Sarkar - calcutta, India - tanmay@kisholoy.org
 );
 
 /** Get current language
