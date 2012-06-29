54f464a893d3ef9300da232c50f995376d487244
Jakub Vrana <jakub@vrana.cz>
2012-06-29 15:23:35 -0700

    Set character set in SQLSRV (bug #3479986)

--- adminer/drivers/mssql.inc.php
+++ adminer/drivers/mssql.inc.php
@@ -23,7 +23,7 @@ if (isset($_GET["mssql"])) {
 			}
 
 			function connect($server, $username, $password) {
-				$this->_link = @sqlsrv_connect($server, array("UID" => $username, "PWD" => $password));
+				$this->_link = @sqlsrv_connect($server, array("UID" => $username, "PWD" => $password, "CharacterSet" => "UTF-8"));
 				if ($this->_link) {
 					$info = sqlsrv_server_info($this->_link);
 					$this->server_info = $info['SQLServerVersion'];
