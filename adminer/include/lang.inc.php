729884b94ab8f92b179e22df0872bfbb51d37aa2
Nikola Radovanović <cobisimo@gmail.com>
2012-08-24 11:52:10 +0200

    Serbian translation

--- adminer/include/lang.inc.php
+++ adminer/include/lang.inc.php
@@ -29,6 +29,7 @@ $langs = array(
 	'bn' => 'বাংলা', // Dipak Kumar - dipak.ndc@gmail.com
 	'ar' => 'العربية', // Y.M Amine - Algeria - nbr7@live.fr
 	'fa' => 'فارسی', // mojtaba barghbani - Iran - mbarghbani@gmail.com
+	'sr' => 'српски', // Nikola Radovanović - cobisimo@gmail.com
 );
 
 /** Get current language
@@ -54,7 +55,7 @@ function lang($idf, $number = null) {
 			: ($LANG == 'pl' ? ($number % 10 > 1 && $number % 10 < 5 && $number / 10 % 10 != 1 ? 1 : 2) // different forms for 1, 2-4, other
 			: ($LANG == 'sl' ? ($number % 100 == 1 ? 0 : ($number % 100 == 2 ? 1 : ($number % 100 == 3 || $number % 100 == 4 ? 2 : 3))) // different forms for 1, 2, 3-4, other
 			: ($LANG == 'lt' ? ($number % 10 == 1 && $number % 100 != 11 ? 0 : ($number % 10 > 1 && $number / 10 % 10 != 1 ? 1 : 2)) // different forms for 1, 12-19, other
-			: ($LANG == 'ru' || $LANG == 'uk' ? ($number % 10 == 1 && $number % 100 != 11 ? 0 : ($number % 10 > 1 && $number % 10 < 5 && $number / 10 % 10 != 1 ? 1 : 2)) // different forms for 1, 2-4, other
+			: ($LANG == 'ru' || $LANG == 'sr' || $LANG == 'uk' ? ($number % 10 == 1 && $number % 100 != 11 ? 0 : ($number % 10 > 1 && $number % 10 < 5 && $number / 10 % 10 != 1 ? 1 : 2)) // different forms for 1, 2-4, other
 			: 1
 		))))))); // http://www.gnu.org/software/gettext/manual/html_node/Plural-forms.html
 		$translation = $translation[$pos];
