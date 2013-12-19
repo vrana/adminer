f41ffd9ee1ce91e0e96e9fcde0095f9a2720a61f
Jakub Vrana <jakub@vrana.cz>
2013-12-19 09:31:37 -0800

    Open external links to a new window

--- adminer/include/adminer.inc.php
+++ adminer/include/adminer.inc.php
@@ -9,7 +9,7 @@ class Adminer {
 	* @return string HTML code
 	*/
 	function name() {
-		return "<a href='http://www.adminer.org/' id='h1'>Adminer</a>";
+		return "<a href='http://www.adminer.org/' target='_blank' id='h1'>Adminer</a>";
 	}
 
 	/** Connection parameters
@@ -756,7 +756,7 @@ username.form['auth[driver]'].onchange();
 		?>
 <h1>
 <?php echo $this->name(); ?> <span class="version"><?php echo $VERSION; ?></span>
-<a href="http://www.adminer.org/#download" id="version"><?php echo (version_compare($VERSION, $_COOKIE["adminer_version"]) < 0 ? h($_COOKIE["adminer_version"]) : ""); ?></a>
+<a href="http://www.adminer.org/#download" target="_blank" id="version"><?php echo (version_compare($VERSION, $_COOKIE["adminer_version"]) < 0 ? h($_COOKIE["adminer_version"]) : ""); ?></a>
 </h1>
 <?php
 		if ($missing == "auth") {
