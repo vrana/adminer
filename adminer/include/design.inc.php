9995e35659e299e8a244e5b93433689ad9e0e8fc
Jakub Vrana <jakub@vrana.cz>
2014-01-07 13:03:48 -0800

    Update JUSH

--- adminer/include/design.inc.php
+++ adminer/include/design.inc.php
@@ -23,6 +23,7 @@ function page_header($title, $error = "", $breadcrumb = array(), $title2 = "") {
 <script type="text/javascript" src="../adminer/static/functions.js"></script>
 <script type="text/javascript" src="static/editing.js"></script>
 <script type="text/javascript" src="../externals/jush/modules/jush.js"></script>
+<script type="text/javascript" src="../externals/jush/modules/jush-textarea.js"></script>
 <script type="text/javascript" src="../externals/jush/modules/jush-<?php echo $jush; ?>.js"></script>
 <?php if ($adminer->head()) { ?>
 <link rel="shortcut icon" type="image/x-icon" href="../adminer/static/favicon.ico">
