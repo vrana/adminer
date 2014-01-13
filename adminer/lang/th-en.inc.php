ebc479d3ae887cc77416254d68d2c86698635b95
vvver <suntorn.mon@gmail.com>
2014-01-13 08:13:20 +0700

    Create th-en.inc.php

---
+++ adminer/lang/th-en.inc.php
@@ -0,0 +1,22 @@
+<?php
+$translations = array(
+'Logged as: %s' => 'User Sign in: %s',
+'Logout successful.' => 'Logout successful (:',
+'Dump' => 'Export',
+'Alter database' => 'Edit database',
+'New item' => 'Insert table',
+'Alter table' => 'Edit table',
+'Modify' => 'Edit all',
+'Drop' => 'Delete',
+'Truncate' => 'Delete rows all (truncate)',
+'Alter indexes' => 'Edit indexes',
+'Alter' => 'Edit',
+'Tables' => 'Tables Number',
+'Rows' => 'Rows Number',
+'SQL command' => 'SQL command',
+'Alter schema' => 'Edit schema',
+'Permanent login' => 'Remember Login',
+'System' => 'System type',
+'No extension' => 'No extension !',
+);
+ 
