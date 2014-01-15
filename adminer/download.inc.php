bb7331e6a0c52bb8737fca94cbd4afc43c2e2570
Jakub Vrana <jakub@vrana.cz>
2014-01-15 08:26:47 -0800

    MongoDB, Elasticsearch: Allow downloading binary data

--- adminer/download.inc.php
+++ adminer/download.inc.php
@@ -3,5 +3,8 @@ $TABLE = $_GET["download"];
 $fields = fields($TABLE);
 header("Content-Type: application/octet-stream");
 header("Content-Disposition: attachment; filename=" . friendly_url("$TABLE-" . implode("_", $_GET["where"])) . "." . friendly_url($_GET["field"]));
-echo $connection->result("SELECT" . limit(idf_escape($_GET["field"]) . " FROM " . table($TABLE), " WHERE " . where($_GET, $fields), 1));
+$select = array(idf_escape($_GET["field"]));
+$result = $driver->select($TABLE, $select, array(where($_GET, $fields)), $select);
+$row = ($result ? $result->fetch_row() : array());
+echo $row[0];
 exit; // don't output footer
