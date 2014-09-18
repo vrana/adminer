6eb177dc22b1d29c4f8feda26ea0949ca4a832c5
Zemistr <zemistr@gmail.com>
2014-09-18 12:09:54 +0200

    Improved plugin to display JSON values ​​as the table in edit form

--- plugins/json-column.php
+++ plugins/json-column.php
@@ -1,21 +1,68 @@
 <?php
 
 /** Display JSON values as table in edit
-* @link http://www.adminer.org/plugins/#use
-* @author Jakub Vrana, http://www.vrana.cz/
-* @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
-* @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
-*/
+ *
+ * @link    http://www.adminer.org/plugins/#use
+ * @author  Jakub Vrana, http://www.vrana.cz/
+ * @author  Martin Zeman (Zemistr), http://www.zemistr.eu/
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
+ */
 class AdminerJsonColumn {
-	
-	function editInput($table, $field, $attrs, $value) {
-		if (substr($value, 0, 1) == '{' && ($json = json_decode($value, true))) {
-			echo "<table cellspacing='0'>";
-			foreach ($json as $key => $val) {
-				echo "<tr><th>" . h($key) . "<td><code class='jush-js'>" . h(json_encode($val)) . "</code>";
-			}
-			echo "</table>";
+	private function __testJson($value) {
+		if ((substr($value, 0, 1) == '{' || substr($value, 0, 1) == '[') && ($json = json_decode($value, true))) {
+			return $json;
 		}
+
+		return $value;
 	}
 
+	private function __buildTable($json) {
+		echo '<table cellspacing="0" style="margin:2px">';
+
+		foreach ($json as $key => $val) {
+			echo '<tr>';
+			echo '<th>' . h($key) . '</th>';
+
+			echo '<td>';
+
+			if (is_scalar($val) || $val === null) {
+				switch (true) {
+					case is_numeric($val):
+						break;
+
+					case is_bool($val):
+						$val = $val ? 'true' : 'false';
+						break;
+
+					case $val === null:
+						$val = 'null';
+						break;
+
+					default:
+						$val = '"' . h($val) . '"';
+						break;
+				}
+
+				echo '<code class="jush-js">' . $val . '</code>';
+			}
+			else {
+				$this->__buildTable($val);
+			}
+
+			echo '</td>';
+
+			echo '</tr>';
+		}
+
+		echo '</table>';
+	}
+
+	function editInput($table, $field, $attrs, $value) {
+		$json = $this->__testJson($value);
+
+		if ($json !== $value) {
+			$this->__buildTable($json);
+		}
+	}
 }
