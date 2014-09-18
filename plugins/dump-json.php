e95fbf34142d6cf7f49185e1aa54bdc0ca940ac0
Zemistr <zemistr@gmail.com>
2014-09-18 12:07:44 +0200

    Re-Created plugin for dump to JSON

--- plugins/dump-json.php
+++ plugins/dump-json.php
@@ -1,62 +1,63 @@
 <?php
 
 /** Dump to JSON format
-* @link http://www.adminer.org/plugins/#use
-* @author Jakub Vrana, http://www.vrana.cz/
-* @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
-* @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
-*/
+ *
+ * @link    http://www.adminer.org/plugins/#use
+ * @author  Martin Zeman (Zemistr), http://www.zemistr.eu/
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
+ */
 class AdminerDumpJson {
-	/** @access protected */
-	var $database = false;
-	
+	var $output = array();
+	var $shutdown_callback = false;
+
 	function dumpFormat() {
 		return array('json' => 'JSON');
 	}
 
-	function dumpTable($table, $style, $is_view = false) {
-		if ($_POST["format"] == "json") {
-			return true;
-		}
-	}
-	
-	function _database() {
-		echo "}\n";
-	}
-	
-	function dumpData($table, $style, $query) {
-		if ($_POST["format"] == "json") {
-			if ($this->database) {
-				echo ",\n";
-			} else {
-				$this->database = true;
-				echo "{\n";
-				register_shutdown_function(array($this, '_database'));
-			}
-			$connection = connection();
-			$result = $connection->query($query, 1);
-			if ($result) {
-				echo '"' . addcslashes($table, "\r\n\"\\") . "\": [\n";
-				$first = true;
-				while ($row = $result->fetch_assoc()) {
-					echo ($first ? "" : ", ");
-					$first = false;
-					foreach ($row as $key => $val) {
-						json_row($key, $val);
-					}
-					json_row("");
-				}
-				echo "]";
-			}
-			return true;
-		}
-	}
-
-	function dumpHeaders($identifier, $multi_table = false) {
+	function dumpHeaders() {
 		if ($_POST["format"] == "json") {
 			header("Content-Type: application/json; charset=utf-8");
+
 			return "json";
 		}
 	}
 
+	function dumpDatabase() {
+		if ($_POST['format'] == 'json') {
+			if (!$this->shutdown_callback) {
+				$this->shutdown_callback = true;
+				register_shutdown_function(array($this, '_export'));
+			}
+
+			return true;
+		}
+	}
+
+	function dumpTable($table) {
+		if ($_POST['format'] == 'json') {
+			$this->output[$table] = array();
+
+			return true;
+		}
+	}
+
+	function dumpData($table, $style, $query) {
+		if ($_POST['format'] == 'json') {
+			$connection = connection();
+			$result = $connection->query($query, 1);
+
+			if ($result) {
+				while ($row = $result->fetch_assoc()) {
+					$this->output[$table][] = $row;
+				}
+			}
+
+			return true;
+		}
+	}
+
+	function _export() {
+		echo json_encode($this->output, 128);
+	}
 }
