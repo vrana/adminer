a062e9cc588bfa008481856503155bc9a833f379
Adam Kuśmierz <kusmierz@users.noreply.github.com>
2016-06-29 18:27:34 +0200

    Checkbox for bool in psql

--- adminer/include/functions.inc.php
+++ adminer/include/functions.inc.php
@@ -890,6 +890,9 @@ function input($field, $value, $function) {
 		$input = $adminer->editInput($_GET["edit"], $field, $attrs, $value); // usage in call is without a table
 		if ($input != "") {
 			echo $input;
+		} elseif (preg_match('~bool~', $field["type"])) {
+			echo "<input type='hidden'$attrs value='0'>" .
+				"<input type='checkbox'" . (in_array(strtolower($value), array('1',  't',  'true',  'y',  'yes',  'on')) ? " checked='checked'" : "") . "$attrs value='1'>";
 		} elseif ($field["type"] == "set") { //! 64 bits
 			preg_match_all("~'((?:[^']|'')*)'~", $field["length"], $matches);
 			foreach ($matches[1] as $i => $val) {
