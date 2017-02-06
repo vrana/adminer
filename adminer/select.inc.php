1bc2a52b801fb86330953dee48522225a4815e19
Stanislav Škubal <stana@folklorista.cz>
2017-02-06 13:58:10 +0100

    PostgreSQL cross schema foreign key
    Fixed issue https://sourceforge.net/p/adminer/bugs-and-features/378/

--- adminer/select.inc.php
+++ adminer/select.inc.php
@@ -387,6 +387,8 @@ if (!$columns && support("table")) {
 										$link .= where_link($i, $foreign_key["target"][$i], $rows[$n][$source]);
 									}
 									$link = ($foreign_key["db"] != "" ? preg_replace('~([?&]db=)[^&]+~', '\\1' . urlencode($foreign_key["db"]), ME) : ME) . 'select=' . urlencode($foreign_key["table"]) . $link; // InnoDB supports non-UNIQUE keys
+									preg_match_all('/REFERENCES (.*)\.(.*)\(/', $foreign_key["definition"], $schemas);
+									$link = ($schemas[1][0] !== null ? preg_replace('~([?&]ns=)[^&]+~', '\\1' . urlencode($schemas[1][0]), $link) : $link); // PostgreSQL cross schema foreign key
 									if (count($foreign_key["source"]) == 1) {
 										break;
 									}
