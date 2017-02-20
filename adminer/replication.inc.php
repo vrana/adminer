4588af77accecf0e74a5f6f978ae9b899d8e3d17
Jakub Vrana <jakub@vrana.cz>
2017-02-20 14:06:19 +0100

    Display message about missing master status

--- adminer/replication.inc.php
+++ adminer/replication.inc.php
@@ -1,20 +1,24 @@
 <?php
 page_header(lang('Replication'));
 
-echo "<p><b>" . lang('Master status') . "</b>" . doc_link(array("sql" => "show-master-status.html")) . "\n";
+echo "<h3>" . lang('Master status') . doc_link(array("sql" => "show-master-status.html")) . "</h3>\n";
 $master_replication_status = replication_status("MASTER");
-echo "<table cellspacing='0'>\n";
-foreach ($master_replication_status[0] as $key => $val) {
-	echo "<tr>";
-	echo "<th>" . h($key);
-	echo "<td>" . nbsp($val);
+if (!$master_replication_status) {
+	echo "<p class='message'>" . lang('No rows.') . "\n";
+} else {
+	echo "<table cellspacing='0'>\n";
+	foreach ($master_replication_status[0] as $key => $val) {
+		echo "<tr>";
+		echo "<th>" . h($key);
+		echo "<td>" . nbsp($val);
+	}
+	echo "</table>\n";
 }
-echo "</table>\n";
 
 $slave_replication_status = replication_status("SLAVE");
-if (!empty($slave_replication_status)) {
-	echo "<p><b>" . lang('Slave status') . "</b>" . doc_link(array("sql" => "show-slave-status.html")) . "\n";
-	foreach ($slave_replication_status as $slave) {
+if ($slave_replication_status) {
+	echo "<h3>" . lang('Slave status') . doc_link(array("sql" => "show-slave-status.html")) . "</h3>\n";
+	foreach ($slave_replication_status[0] as $slave) {
 		echo "<table cellspacing='0'>\n";
 		foreach ($slave as $key => $val) {
 			echo "<tr>";
