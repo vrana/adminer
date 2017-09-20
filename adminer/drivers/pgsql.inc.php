3407f59a02726082dbf3e53a34f78fac3cd8d153
Josh <mail@joshlrogers.com>
2017-09-20 12:57:42 -0700

    favor schema in querystring over current_schema() for navigation

--- adminer/drivers/pgsql.inc.php
+++ adminer/drivers/pgsql.inc.php
@@ -287,10 +287,12 @@ ORDER BY 1";
 	}
 
 	function table_status($name = "") {
+        global $connection;
+        $schema = !empty($_GET['ns']) ? "'" . pg_escape_string($connection->_link, $_GET['ns']) . "'" : 'current_schema()';
 		$return = array();
 		foreach (get_rows("SELECT c.relname AS \"Name\", CASE c.relkind WHEN 'r' THEN 'table' WHEN 'm' THEN 'materialized view' ELSE 'view' END AS \"Engine\", pg_relation_size(c.oid) AS \"Data_length\", pg_total_relation_size(c.oid) - pg_relation_size(c.oid) AS \"Index_length\", obj_description(c.oid, 'pg_class') AS \"Comment\", c.relhasoids::int AS \"Oid\", c.reltuples as \"Rows\", n.nspname
 FROM pg_class c
-JOIN pg_namespace n ON(n.nspname = current_schema() AND n.oid = c.relnamespace)
+JOIN pg_namespace n ON(n.nspname = $schema AND n.oid = c.relnamespace)
 WHERE relkind IN ('r', 'm', 'v')
 " . ($name != "" ? "AND relname = " . q($name) : "ORDER BY relname")
 		) as $row) { //! Index_length, Auto_increment
