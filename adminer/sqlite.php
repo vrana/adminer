b3d5c9affe72a0e4ebdce68d3eb5ef80016b1769
Jakub Vrana <jakub@vrana.cz>
2018-01-16 16:37:59 +0100

    Require specifying credentials in login-sqlite plugin

--- adminer/sqlite.php
+++ adminer/sqlite.php
@@ -2,7 +2,7 @@
 function adminer_object() {
 	include_once "../plugins/plugin.php";
 	include_once "../plugins/login-sqlite.php";
-	return new AdminerPlugin(array(new AdminerLoginSqlite));
+	return new AdminerPlugin(array(new AdminerLoginSqlite("admin", password_hash("", PASSWORD_DEFAULT))));
 }
 
 include "./index.php";
--- plugins/login-sqlite.php
+++ plugins/login-sqlite.php
@@ -1,36 +1,29 @@
 <?php
 
-/** Enable auto-login for SQLite
+/** Enable login for SQLite
 * @link https://www.adminer.org/plugins/#use
 * @author Jakub Vrana, https://www.vrana.cz/
 * @license https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
 * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
 */
 class AdminerLoginSqlite {
-
+	var $login;
+	var $password_hash;
+	
+	/** 
+	* @param string
+	* @param string result of password_hash
+	*/
+	function AdminerLoginSqlite($login, $password_hash) {
+		$this->login = $login;
+		$this->password_hash = $password_hash;
+	}
+	
 	function login($login, $password) {
-		return true;
-	}
-
-	function loginForm() {
-		?>
-<script<?php echo nonce(); ?>>
-addEventListener('load', function () {
-	var driver = qs('name="auth[driver]"');
-	if (isTag(driver, 'select')) {
-		driver.onchange = function () {
-			var trs = parentTag(driver, 'table').rows;
-			for (var i=1; i < trs.length - 1; i++) {
-				var disabled = /sqlite/.test(driver.value);
-				alterClass(trs[i], 'hidden', disabled);
-				trs[i].querySelector('input').disabled = disabled;
-			}
-		};
-	}
-	driver.onchange();
-});
-</script>
-<?php
+		if (DRIVER != "sqlite" && DRIVER != "sqlite2") {
+			return true;
+		}
+		return $this->login == $login && password_verify($password, $this->password_hash);
 	}
 
 }
