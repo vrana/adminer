vrana/adminer

commit 150301ed5f7c15ac7edf329c9a7101b5c4497acd

Allow using  in ->login() (bug #381)

adminer/include/auth.inc.php
@@ -137,12 +137,12 @@ if (isset($_GET["username"])) {
 	$connection = connect();
 }
 
+$driver = new Min_Driver($connection);
+
 if (!is_object($connection) || !$adminer->login($_GET["username"], get_password())) {
 	auth_error();
 	exit;
 }
-
-$driver = new Min_Driver($connection);
 
 if ($auth && $_POST["token"]) {
 	$_POST["token"] = $token; // reset token after explicit login

commit 3b4ce4a0b4281ca74ea9921eec314edaa5b5444d

Fix single driver non-MySQL compiled versions (regression from 150301ed)

adminer/include/auth.inc.php
@@ -171,9 +171,8 @@ if (isset($_GET["username"])) {
 	}
 	check_invalid_login();
 	$connection = connect();
+	$driver = new Min_Driver($connection);
 }
-
-$driver = new Min_Driver($connection);
 
 if (!is_object($connection) || ($login = $adminer->login($_GET["username"], get_password())) !== true) {
 	auth_error((is_string($connection) ? h($connection) : (is_string($login) ? $login : lang('Invalid credentials.'))));