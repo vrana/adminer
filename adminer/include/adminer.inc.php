c75096f17359768dd1c385832593f3f8d22887f0
Jakub Vrana <jakub@vrana.cz>
2018-02-01 22:22:29 +0100

    Space between DB and Use

--- adminer/include/adminer.inc.php
+++ adminer/include/adminer.inc.php
@@ -986,10 +986,10 @@ bodyLoad('<?php echo (is_object($connection) ? preg_replace('~^(\\d\\.?\\d).*~s'
 <p id="dbs">
 <?php
 		hidden_fields_get();
-		$db_events = script("mixin(qsl('select'), {onmousedown: dbMouseDown, onchange: dbChange});", "");
+		$db_events = script("mixin(qsl('select'), {onmousedown: dbMouseDown, onchange: dbChange});");
 		echo "<span title='" . lang('database') . "'>" . lang('DB') . "</span>: " . ($databases
 			? "<select name='db'>" . optionlist(array("" => "") + $databases, DB) . "</select>$db_events"
-			: '<input name="db" value="' . h(DB) . '" autocapitalize="off">'
+			: "<input name='db' value='" . h(DB) . "' autocapitalize='off'>\n"
 		);
 		echo "<input type='submit' value='" . lang('Use') . "'" . ($databases ? " class='hidden'" : "") . ">\n";
 		if ($missing != "db" && DB != "" && $connection->select_db(DB)) {
