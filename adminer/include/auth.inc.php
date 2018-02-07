cac523402a4bcfe1df37745515a06572723f2416
Jakub Vrana <jakub@vrana.cz>
2018-02-07 11:37:53 +0100

    Display Invalid credentials if connection error is empty

--- adminer/include/auth.inc.php
+++ adminer/include/auth.inc.php
@@ -164,8 +164,9 @@ if (isset($_GET["username"])) {
 	$driver = new Min_Driver($connection);
 }
 
+$login = null;
 if (!is_object($connection) || ($login = $adminer->login($_GET["username"], get_password())) !== true) {
-	auth_error((is_string($connection) ? h($connection) : (is_string($login) ? $login : lang('Invalid credentials.'))));
+	auth_error((is_string($connection) && $connection != "" ? h($connection) : (is_string($login) ? $login : lang('Invalid credentials.'))));
 }
 
 if ($auth && $_POST["token"]) {
