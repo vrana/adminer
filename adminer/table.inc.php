99d7b88341badfc8aed4cd0afc451e45b320e2e4
Jakub Vrana <jakub@vrana.cz>
2018-02-08 10:34:43 +0100

    Use tableName() in table.inc.php

--- adminer/table.inc.php
+++ adminer/table.inc.php
@@ -5,8 +5,9 @@ if (!$fields) {
 	$error = error();
 }
 $table_status = table_status1($TABLE, true);
+$name = $adminer->tableName($table_status);
 
-page_header(($fields && is_view($table_status) ? $table_status['Engine'] == 'materialized view' ? lang('Materialized view') : lang('View') : lang('Table')) . ": " . h($TABLE), $error);
+page_header(($fields && is_view($table_status) ? $table_status['Engine'] == 'materialized view' ? lang('Materialized view') : lang('View') : lang('Table')) . ": " . ($name != "" ? $name : h($TABLE)), $error);
 
 $adminer->selectLinks($table_status);
 $comment = $table_status["Comment"];
