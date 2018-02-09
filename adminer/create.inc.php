07d1bc429132461d2861a4b58d45b2e0913f72cc
Jakub Vrana <jakub@vrana.cz>
2018-02-09 17:45:50 +0100

    Respect unchecked Default values

--- adminer/create.inc.php
+++ adminer/create.inc.php
@@ -186,8 +186,8 @@ edit_fields($row["fields"], $collations, "TABLE", $foreign_keys, $comments);
 </table>
 <p>
 <?php echo lang('Auto Increment'); ?>: <input type="number" name="Auto_increment" size="6" value="<?php echo h($row["Auto_increment"]); ?>">
-<?php echo checkbox("defaults", 1, true, lang('Default values'), "columnShow(this.checked, 5)", "jsonly"); ?>
-<?php if (!$_POST["defaults"]) { echo script("editingHideDefaults();"); } ?>
+<?php echo checkbox("defaults", 1, !$_POST || $_POST["defaults"], lang('Default values'), "columnShow(this.checked, 5)", "jsonly"); ?>
+<?php if (!$_POST) { echo script("editingHideDefaults();"); } ?>
 <?php echo (support("comment")
 	? "<label><input type='checkbox' name='comments' value='1' class='jsonly'" . ($comments ? " checked" : "") . ">" . lang('Comment') . "</label>"
 		. script("qsl('input').onclick = partial(editingCommentsClick, true);")
