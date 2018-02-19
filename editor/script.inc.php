1f5ca321b6136bc6ebaa570e1df9057a5aec6af3
Jakub Vrana <jakub@vrana.cz>
2018-02-19 23:04:27 +0100

    Comment

--- editor/script.inc.php
+++ editor/script.inc.php
@@ -2,7 +2,7 @@
 if ($_GET["script"] == "kill") {
 	$connection->query("KILL " . number($_POST["kill"]));
 
-} elseif (list($table, $id, $name) = $adminer->_foreignColumn(column_foreign_keys($_GET["source"]), $_GET["field"])) {
+} elseif (list($table, $id, $name) = $adminer->_foreignColumn(column_foreign_keys($_GET["source"]), $_GET["field"])) { // complete
 	$limit = 11;
 	$result = $connection->query("SELECT $id, $name FROM " . table($table) . " WHERE " . (preg_match('~^[0-9]+$~', $_GET["value"]) ? "$id = $_GET[value] OR " : "") . "$name LIKE " . q("$_GET[value]%") . " ORDER BY 2 LIMIT $limit");
 	for ($i=1; ($row = $result->fetch_row()) && $i < $limit; $i++) {
