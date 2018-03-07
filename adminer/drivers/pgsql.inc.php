ef0eea1041ec808043e0b712f175bfff48e0caea
Sartor <sartor@syneforge.com>
2018-03-07 11:21:58 +0200

    PgSQL convert search fix

--- adminer/drivers/pgsql.inc.php
+++ adminer/drivers/pgsql.inc.php
@@ -199,8 +199,8 @@ if (isset($_GET["pgsql"])) {
 
 		function convertSearch($idf, $val, $field) {
 			return (preg_match('~char|text' . (is_numeric($val["val"]) && !preg_match('~LIKE~', $val["op"]) ? '|' . number_type() : '') . '~', $field["type"])
-				? $idf
-				: "CAST($idf AS text)"
+				? "CAST($idf AS text)"
+				: $idf
 			);
 		}
 
