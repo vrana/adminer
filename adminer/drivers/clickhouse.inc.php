da76e6b9b8b1e735314eb8a537da494e97bbb1ad
helix84 <helix84@centrum.sk>
2018-12-18 14:21:23 +0100

    php <5.4 compat: array syntax (#313)

--- adminer/drivers/clickhouse.inc.php
+++ adminer/drivers/clickhouse.inc.php
@@ -259,7 +259,7 @@ if (isset($_GET["clickhouse"])) {
 	}
 
 	function unconvert_field($field, $return) {
-		if (in_array($field['type'], ["Int8", "Int16", "Int32", "Int64", "UInt8", "UInt16", "UInt32", "UInt64", "Float32", "Float64"])) {
+		if (in_array($field['type'], array("Int8", "Int16", "Int32", "Int64", "UInt8", "UInt16", "UInt32", "UInt64", "Float32", "Float64"))) {
 			return "to$field[type]($return)";
 		}
 		return $return;
