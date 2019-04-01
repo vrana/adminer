327041874ecdb398436adc8a5d32d49c6866dd8c
Dennis Stolmeijer <dstollie@gmail.com>
2019-04-01 17:57:47 +0200

    Always pass the SRID part of a geometry property with the GeomFromText function

--- adminer/drivers/mysql.inc.php
+++ adminer/drivers/mysql.inc.php
@@ -1079,7 +1079,7 @@ if (!defined("DRIVER")) {
 			$return = "CONV($return, 2, 10) + 0";
 		}
 		if (preg_match("~geometry|point|linestring|polygon~", $field["type"])) {
-			$return = (min_version(8) ? "ST_" : "") . "GeomFromText($return)";
+			$return = (min_version(8) ? "ST_" : "") . "GeomFromText($return, SRID($field[field]))";
 		}
 		return $return;
 	}
