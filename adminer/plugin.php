52ad120648f234db43d0f1498f69cb67eb9a5521
Troy Olson <panreach@gmail.com>
2019-08-01 19:18:01 -0500

    Fix/Enhance AdminerDesigns Plugin to work intuitively.

--- adminer/plugin.php
+++ adminer/plugin.php
@@ -28,6 +28,8 @@ function adminer_object() {
 		new AdminerEnumOption,
 		new AdminerTablesFilter,
 		new AdminerEditForeign,
+		new AdminerDesigns(), # Show all ../designs/* (default)
+		//~ new AdminerDesigns(array("hever", "lucas-sandery", "mancave")), # Or, show limited AdminerDesigns via theme name.
 	);
 	
 	/* It is possible to combine customization and plugins:
