73d01069c0b0eae249472dc6643301364ca089fd
peterdd <ddpm@liscovius.de>
2019-08-17 18:47:58 +0200

    avoid horizontal scrollbar on variables page
    some server variables can get very long without any whitespace or hints for automatic wrapping.

--- adminer/variables.inc.php
+++ adminer/variables.inc.php
@@ -6,7 +6,13 @@ $variables = ($status ? show_status() : show_variables());
 if (!$variables) {
 	echo "<p class='message'>" . lang('No rows.') . "\n";
 } else {
-	echo "<table cellspacing='0'>\n";
+	# some variable values like optimizer_switch on Mariadb can contain nasty long strings without any whitespace 
+	# see https://developer.mozilla.org/en-US/docs/Web/CSS/word-break
+	echo '<style>table.info td {
+	word-break:break-word; /* deprecated, all browsers */
+	overflow-wrap:anywhere; /* CSS3 WD, only firefox in 2019 */
+	}</style>';
+	echo "<table class='info'>\n";
 	foreach ($variables as $key => $val) {
 		echo "<tr>";
 		echo "<th><code class='jush-" . $jush . ($status ? "status" : "set") . "'>" . h($key) . "</code>";
