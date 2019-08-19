4a27882a9759897409b0aea5ee1d54410874aefb
Jakub Vrana <jakub@vrana.cz>
2019-08-19 17:14:27 +0200

    SQLite: Quote strings stored in integer columns in export (bug #696)

--- adminer/include/adminer.inc.php
+++ adminer/include/adminer.inc.php
@@ -845,7 +845,7 @@ class Adminer {
 						foreach ($row as $key => $val) {
 							$field = $fields[$key];
 							$row[$key] = ($val !== null
-								? unconvert_field($field, preg_match(number_type(), $field["type"]) && $val != '' && !preg_match('~\[~', $field["full_type"]) ? $val : q(($val === false ? 0 : $val)))
+								? unconvert_field($field, preg_match(number_type(), $field["type"]) && $val != '' && !preg_match('~\[~', $field["full_type"]) && preg_match('~^[0-9]+$~', $val) ? $val : q(($val === false ? 0 : $val)))
 								: "NULL"
 							);
 						}
