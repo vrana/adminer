965fe2e8ae75815d123dedb2b8bdd5218654337f
Jakub Vrana <jakub@vrana.cz>
2019-08-28 10:31:12 +0200

    Fix PostgreSQL doc root

--- adminer/include/editing.inc.php
+++ adminer/include/editing.inc.php
@@ -531,7 +531,7 @@ function doc_link($paths, $text = "<sup>?</sup>") {
 	$urls = array(
 		'sql' => "https://dev.mysql.com/doc/refman/$version/en/",
 		'sqlite' => "https://www.sqlite.org/",
-		'pgsql' => "https://www.postgresql.org/docs/$version/static/",
+		'pgsql' => "https://www.postgresql.org/docs/$version/",
 		'mssql' => "https://msdn.microsoft.com/library/",
 		'oracle' => "https://www.oracle.com/pls/topic/lookup?ctx=db" . preg_replace('~^.* (\d+)\.(\d+)\.\d+\.\d+\.\d+.*~s', '\1\2', $server_info) . "&id=",
 	);
