230fe22a7f38fff6d475320e8c85ae7768ba74fb
Tim Düsterhus <tim@bastelstu.be>
2020-10-20 23:38:40 +0200

    Fix `Fatal error: Uncaught Error: Call to undefined function get_magic_quotes_gpc()`

--- adminer/include/bootstrap.inc.php
+++ adminer/include/bootstrap.inc.php
@@ -60,7 +60,7 @@ if (!defined("SID")) {
 
 // disable magic quotes to be able to use database escaping function
 remove_slashes(array(&$_GET, &$_POST, &$_COOKIE), $filter);
-if (get_magic_quotes_runtime()) {
+if (function_exists('get_magic_quotes_runtime') && get_magic_quotes_runtime()) {
 	set_magic_quotes_runtime(false);
 }
 @set_time_limit(0); // @ - can be disabled
