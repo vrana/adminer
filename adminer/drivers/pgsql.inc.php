94194aac19e34b19bcc797cb516bb3f37624427c
Jakub Vrana <jakub@vrana.cz>
2021-02-06 17:17:36 +0100

    PostgreSQL: Support smallserial

--- adminer/drivers/pgsql.inc.php
+++ adminer/drivers/pgsql.inc.php
@@ -509,7 +509,7 @@ ORDER BY connamespace, conname") as $row) {
 				$val5 = $val[5];
 				unset($val[5]);
 				if (isset($val[6]) && $field[0] == "") { // auto_increment
-					$val[1] = ($val[1] == " bigint" ? " big" : " ") . "serial";
+					$val[1] = ($val[1] == " bigint" ? " big" : ($val[1] == " smallint" ? " small" : " ")) . "serial";
 				}
 				if ($field[0] == "") {
 					$alter[] = ($table != "" ? "ADD " : "  ") . implode($val);
