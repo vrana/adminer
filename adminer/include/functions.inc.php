31e28fbd00ec39ae7bd56379828739728929d884
Jakub Vrana <jakub@vrana.cz>
2021-02-07 09:41:07 +0100

    Quote values with leading and trailing zeroes in CSV export (bug #777)

--- adminer/include/functions.inc.php
+++ adminer/include/functions.inc.php
@@ -1129,7 +1129,7 @@ function dump_headers($identifier, $multi_table = false) {
 */
 function dump_csv($row) {
 	foreach ($row as $key => $val) {
-		if (preg_match("~[\"\n,;\t]~", $val) || $val === "") {
+		if (preg_match('~["\n,;\t]|^0|\.\d*0$~', $val) || $val === "") {
 			$row[$key] = '"' . str_replace('"', '""', $val) . '"';
 		}
 	}
