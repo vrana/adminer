124092fd7ca1131d579e3b6641aac0adb3597000
Jakub Vrana <jakub@vrana.cz>
2021-02-08 12:26:51 +0100

    MySQL 8: Fix EXPLAIN in SQL command

--- adminer/drivers/mysql.inc.php
+++ adminer/drivers/mysql.inc.php
@@ -950,7 +950,7 @@ if (!defined("DRIVER")) {
 	* @return Min_Result
 	*/
 	function explain($connection, $query) {
-		return $connection->query("EXPLAIN " . (min_version(5.1) ? "PARTITIONS " : "") . $query);
+		return $connection->query("EXPLAIN " . (min_version(5.1) && !min_version(5.7) ? "PARTITIONS " : "") . $query);
 	}
 
 	/** Get approximate number of rows
