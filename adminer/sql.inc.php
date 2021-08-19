74e4bb9a6f1519c090d28555a6c7eaff880d4ec0
Michael Graß <info@mgrass.biz>
2021-08-19 12:33:07 +0200

    Do not limit unlimited memory, fix number conversion warning

--- adminer/sql.inc.php
+++ adminer/sql.inc.php
@@ -32,8 +32,8 @@ if (!$error && $_POST) {
 	}
 
 	if (is_string($query)) { // get_file() returns error as number, fread() as false
-		if (function_exists('memory_get_usage')) {
-			@ini_set("memory_limit", max(ini_bytes("memory_limit"), 2 * strlen($query) + memory_get_usage() + 8e6)); // @ - may be disabled, 2 - substr and trim, 8e6 - other variables
+		if (function_exists('memory_get_usage') && ($memory_limit = ini_bytes("memory_limit")) != "-1") {
+			@ini_set("memory_limit", max($memory_limit, 2 * strlen($query) + memory_get_usage() + 8e6)); // @ - may be disabled, 2 - substr and trim, 8e6 - other variables
 		}
 
 		if ($query != "" && strlen($query) < 1e6) { // don't add big queries
