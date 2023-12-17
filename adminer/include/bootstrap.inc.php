00b9fbda082b2f8cf1abf7b7790f99825539c26c
Sneda8 <85223444+Sneda8@users.noreply.github.com>
2023-12-17 01:36:25 +0100

    PHP 8.3 error suppression
    PHP 8.3 has shortened the array access on null error message to "Trying to access array offset on null". This commit changes the regular expression used to circumvent errors.

--- adminer/include/bootstrap.inc.php
+++ adminer/include/bootstrap.inc.php
@@ -1,6 +1,6 @@
 <?php
 function adminer_errors($errno, $errstr) {
-	return !!preg_match('~^(Trying to access array offset on value of type null|Undefined array key)~', $errstr);
+	return (bool)preg_match('~^(Trying to access array offset on( value of type)? null|Undefined array key)~', $errstr);
 }
 
 error_reporting(6135); // errors and warnings
