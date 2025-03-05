f5d47d46a09ba97f49770c9f45c87b9c14592edd
Jakub Vrana <jakub@vrana.cz>
2025-03-05 17:08:29 +0100

    Use anonymous function

--- adminer/include/errors.inc.php
+++ adminer/include/errors.inc.php
@@ -1,9 +1,7 @@
 <?php
 namespace Adminer;
 
-function adminer_errors($errno, $errstr) {
-	return !!preg_match('~^(Trying to access array offset on( value of type)? null|Undefined (array key|property))~', $errstr);
-}
-
 error_reporting(6135); // errors and warnings
-set_error_handler('Adminer\adminer_errors', E_WARNING);
+set_error_handler(function ($errno, $errstr) {
+	return !!preg_match('~^(Trying to access array offset on( value of type)? null|Undefined (array key|property))~', $errstr);
+}, E_WARNING);
