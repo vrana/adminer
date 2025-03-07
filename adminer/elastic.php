2c9f380c64ec84a1dc44745df29c5fbf1081fcb0
Jakub Vrana <jakub@vrana.cz>
2025-03-07 12:17:14 +0100

    Elastic: Remove plugin for version 5

--- adminer/elastic.php
+++ adminer/elastic.php
@@ -2,14 +2,8 @@
 // To create Adminer just for Elasticsearch, run `../compile.php elastic`.
 
 function adminer_object() {
-	include_once "../plugins/plugin.php";
-	include_once "../plugins/login-password-less.php";
 	include_once "../plugins/drivers/elastic.php";
-	include_once "../plugins/drivers/elastic5.php";
-	return new AdminerPlugin(array(
-		// TODO: inline the result of password_hash() so that the password is not visible in source codes
-		new AdminerLoginPasswordLess(password_hash("YOUR_PASSWORD_HERE", PASSWORD_DEFAULT)),
-	));
+	return new Adminer\Adminer;
 }
 
 include "./index.php";
