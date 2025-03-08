a1c3e34e0474eb7250d6cd9f7735fa7b79290fec
Jakub Vrana <jakub@vrana.cz>
2025-03-08 17:06:30 +0100

    Translations: check fullstops in 'ja' and 'he'

--- lang.php
+++ lang.php
@@ -35,15 +35,19 @@ foreach (glob(__DIR__ . "/adminer/lang/" . ($_SESSION["lang"] ?: "*") . ".inc.ph
 	preg_match_all("~^(\\s*(?:// [^'].*\\s+)?)(?:// )?(('(?:[^\\\\']+|\\\\.)*') => (.*[^,\n])),?~m", $file, $matches, PREG_SET_ORDER | PREG_OFFSET_CAPTURE);
 	$s = "";
 	$lang = basename($filename, ".inc.php");
-	$fullstop = ($lang == "bn" ? '।' : (substr($lang, 0, 2) == 'zh' ? '。' : ($lang == 'he' || $lang == 'ja' ? '' : '\.')));
+	$fullstop = ($lang == "bn" ? '।' : (preg_match('~^(ja|zh)~', $lang) ? '。' : ($lang == 'he' ? '[^.]' : '\.')));
 	foreach ($matches as $match) {
 		list(, list($indent), list($line, $offset), list($en), list($translation)) = $match;
 		if (isset($messages[$en])) {
 			// keep current messages
 			$s .= "$indent$line,\n";
 			unset($messages[$en]);
-			if ($en != "','" && $fullstop && (substr($en, -2, 1) == "." xor preg_match("~$fullstop'\)?\$~", $line))) {
-				echo "$filename:" . (substr_count($file, "\n", 0, $offset) + 1) . ":Not matching fullstop: $line\n";
+			$en_fullstop = (substr($en, -2, 1) == ".");
+			//! check in array
+			if ($en != "','" && ($en_fullstop xor preg_match("~($fullstop)'\)?\$~", $line, $match))) {
+				if ($lang != ($en_fullstop ? "ja" : "he")) { // fullstop is optional in 'ja', forbidden in 'he'
+					echo "$filename:" . (substr_count($file, "\n", 0, $offset) + 1) . ":Not matching fullstop: $line\n";
+				}
 			}
 			if (preg_match('~%~', $en) xor preg_match('~%~', $translation)) {
 				echo "$filename:" . (substr_count($file, "\n", 0, $offset) + 1) . ":Not matching placeholder.\n";
@@ -54,14 +58,14 @@ foreach (glob(__DIR__ . "/adminer/lang/" . ($_SESSION["lang"] ?: "*") . ".inc.ph
 		}
 	}
 	if ($messages) {
-		if (basename($filename) != "en.inc.php") {
+		if ($lang != "en") {
 			$s .= "\n";
 		}
 		foreach ($messages as $idf => $val) {
 			// add new messages
 			if ($val == "," && strpos($idf, "%d")) {
 				$s .= "\t$idf => array(),\n";
-			} elseif (basename($filename) != "en.inc.php") {
+			} elseif ($lang != "en") {
 				$s .= "\t$idf => null,\n";
 			}
 		}
