156839142e5c8655689d0aa36748e38b65c0b291
Jakub Vrana <jakub@vrana.cz>
2025-03-11 18:16:44 +0100

    Fix plugin AdminerPrettyJsonColumn

--- plugins/pretty-json-column.php
+++ plugins/pretty-json-column.php
@@ -3,13 +3,6 @@
 /** Pretty print JSON values in edit
 */
 class AdminerPrettyJsonColumn {
-	/** @var AdminerPlugin */
-	protected $adminer;
-
-	public function __construct($adminer) {
-		$this->adminer = $adminer;
-	}
-
 	private function testJson($value) {
 		if ((substr($value, 0, 1) == '{' || substr($value, 0, 1) == '[') && ($json = json_decode($value, true))) {
 			return $json;
@@ -33,6 +26,5 @@ class AdminerPrettyJsonColumn {
 				$value = json_encode($json);
 			}
 		}
-		return $this->adminer->_callParent('processInput', array($field, $value, $function));
 	}
 }
