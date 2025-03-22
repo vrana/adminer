ab44927442a55c9153b4137d6af0b66317ef3a9e
Jakub Vrana <jakub@vrana.cz>
2025-03-22 07:22:06 +0100

    Tests: Run also in PDO

---
+++ tests/generate-pdo.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+// Katalon Recorder has global variables: https://docs.katalon.com/katalon-platform/plugins-and-add-ons/katalon-recorder-extension/get-your-job-done/automate-scenarios/global-variables-in-katalon-recorder
+// It's possible to use them in URL in Katalon Studio but apparently not in Recorder: https://forum.katalon.com/t/45673/2
+
+foreach (glob("*.html") as $filename) {
+	if (!preg_match('~^pdo-~', $filename)) {
+		$file = file_get_contents($filename);
+		$file = preg_replace_callback('~/(adminer(/sqlite)?|editor/example)\.php(\??)~', function ($match) {
+			return "/$match[1].php?ext=pdo" . ($match[3] ? "&amp;" : "");
+		}, $file);
+		$file = str_replace("<tr><td>open</td><td>/coverage.php?coverage=0</td><td></td></tr>\n", "", $file);
+		$file = preg_replace("~//input\[@value='Login']~", "\\0</td><td></td></tr>\n<tr><td>verifyTextPresent</td><td>PDO_", $file, 1);
+		file_put_contents("pdo-$filename", $file);
+	}
+}
