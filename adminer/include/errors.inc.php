708646156a0207b23f5c610b1aab559418239062
Jakub Vrana <jakub@vrana.cz>
2025-03-23 22:11:05 +0100

    Notices: Enable E_NOTICE and E_STRICT

--- adminer/include/errors.inc.php
+++ adminer/include/errors.inc.php
@@ -1,7 +1,7 @@
 <?php
 namespace Adminer;
 
-error_reporting(6135); // errors and warnings
+error_reporting(24575); // all but E_DEPRECATED (overriding mysqli methods without types is deprecated)
 set_error_handler(function ($errno, $errstr) {
 	return !!preg_match('~^(Trying to access array offset on( value of type)? null|Undefined (array key|property))~', $errstr);
-}, E_WARNING);
+}, E_WARNING | E_NOTICE); // warning since PHP 8.0
