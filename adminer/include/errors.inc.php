9b84908b99150f957fdd2a71c5189c6a87e80f8f
Jakub Vrana <jakub@vrana.cz>
2025-03-24 06:08:07 +0100

    Notices: Require declared properties

--- adminer/include/errors.inc.php
+++ adminer/include/errors.inc.php
@@ -3,5 +3,5 @@ namespace Adminer;
 
 error_reporting(24575); // all but E_DEPRECATED (overriding mysqli methods without types is deprecated)
 set_error_handler(function ($errno, $errstr) {
-	return !!preg_match('~^(Trying to access array offset on( value of type)? null|Undefined (array key|property))~', $errstr);
+	return !!preg_match('~^(Trying to access array offset on( value of type)? null|Undefined array key)~', $errstr);
 }, E_WARNING | E_NOTICE); // warning since PHP 8.0
