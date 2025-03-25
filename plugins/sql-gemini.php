4bbbea2fbe2d878036cb12bf5c38e464df2acd82
Jakub Vrana <jakub@vrana.cz>
2025-03-25 06:49:34 +0100

    AdminerSqlGemini: Wrap returned text to comment

--- plugins/sql-gemini.php
+++ plugins/sql-gemini.php
@@ -38,7 +38,14 @@ class AdminerSqlGemini {
 			)));
 			$response = json_decode(file_get_contents("https://generativelanguage.googleapis.com/v1beta/models/$this->model:generateContent?key=$this->apiKey", false, $context));
 			$text = $response->candidates[0]->content->parts[0]->text;
-			echo preg_replace('~```sql\n(.*\n)```~sU', '\1', $text) . "\n";
+			$in_code = false;
+			foreach (preg_split('~(^|\n)```(sql)?(\n|$)~', $text) as $part) {
+				$part = trim($part);
+				if ($part) {
+					echo ($in_code ? $part : "/*\n$part\n*/") . "\n\n";
+				}
+				$in_code = !$in_code;
+			}
 			exit;
 		}
 	}
