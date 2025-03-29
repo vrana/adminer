4b262ededafd0124e66114cdfce3e4c9576c4190
Jakub Vrana <jakub@vrana.cz>
2025-03-29 23:20:05 +0100

    Remove unnecessary braces

--- adminer/include/errors.inc.php
+++ adminer/include/errors.inc.php
@@ -5,5 +5,5 @@ error_reporting(24575); // all but E_DEPRECATED (overriding mysqli methods witho
 set_error_handler(function ($errno, $errstr) {
 	// "Undefined array key" mutes $_GET["q"] if there's no ?q=
 	// "Undefined offset" and "Undefined index" are older messages for the same thing
-	return !!preg_match('~^(Undefined (array key|offset|index))~', $errstr);
+	return !!preg_match('~^Undefined (array key|offset|index)~', $errstr);
 }, E_WARNING | E_NOTICE); // warning since PHP 8.0
