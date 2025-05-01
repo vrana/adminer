1862b846123f02f521d0aae5f6c2ea5538d073df
schucan <schucan@users.noreply.github.com>
2025-05-01 16:14:00 +0200

    Mute chmod warning

--- adminer/include/functions.inc.php
+++ adminer/include/functions.inc.php
@@ -685,7 +685,7 @@ function file_open_lock(string $filename) {
 	if (!$fp) {
 		return;
 	}
-	chmod($filename, 0660);
+	@chmod($filename, 0660); // @ - may not be permitted
 	if (!flock($fp, LOCK_EX)) {
 		fclose($fp);
 		return;
