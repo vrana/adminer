466eceff4059a85ce1998992a5c79d90e634c56a
Jakub Vrana <jakub@vrana.cz>
2025-06-19 09:12:04 +0200

    Rename highlight plugins

--- plugins/highlight-codemirror.php
+++ plugins/highlight-codemirror.php
@@ -7,7 +7,7 @@
 * @license https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
 * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
 */
-class AdminerCodemirror extends Adminer\Plugin {
+class AdminerHighlightCodemirror extends Adminer\Plugin {
 	private $root;
 	private $minified;
 
