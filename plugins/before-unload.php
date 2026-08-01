39e4bccc30eb733438a5f155cb2e5c8deb4b0c67
Michal Landsman <landsman@insuit.cz>
2026-08-01 14:20:42 +0200

    before-unload: say the page is being left, not reloaded, in cs and de

--- plugins/before-unload.php
+++ plugins/before-unload.php
@@ -33,8 +33,8 @@ onbeforeunload = () => editChanged;
 	}
 
 	protected $translations = array(
-		'cs' => array('' => 'Zobrazí potvrzení před odnahráním stránky, pokud bylo změněno formulářové políčko'),
-		'de' => array('' => 'Zeigt eine Bestätigung an bevor die Seite neu geladen wird, wenn ein Formularfeld geändert wurde'),
+		'cs' => array('' => 'Zobrazí potvrzení před opuštěním stránky, pokud bylo změněno formulářové políčko'),
+		'de' => array('' => 'Zeigt eine Bestätigung an bevor die Seite verlassen wird, wenn ein Formularfeld geändert wurde'),
 		'ja' => array('' => 'フォームの列が変更された時、ページを再読込みする前に確認を表示'),
 		'pl' => array('' => 'Wyświetlaj potwierdzenie przed rozładowaniem strony, jeśli pole formularza zostało zmienione'),
 		'hr' => array('' => 'Prikazuje potvrdu prije napuštanja stranice ako je polje obrasca promijenjeno'),
