fd672f280f00d824e884d92e00f5c4843d35b8b0
Jakub Vrana <jakub@vrana.cz>
2026-08-31 09:55:18 +0200

    Compile: Do not report connection_id() as missing in the drivers limiting the execution time

--- compile.php
+++ compile.php
@@ -101,6 +101,9 @@ function put_file($match) {
 			unset($functions["__construct"], $functions["__destruct"], $functions["set_charset"], $functions["multi_query"], $functions["store_result"], $functions["next_result"]);
 			unset($functions["inTransaction"]); // SqlDb provides a default implementation, MySQL overrides it only because its Db extends \MySQLi
 			unset($functions["parse_type"], $functions["trigger_event"]); // helpers of the MySQL driver, not a part of the driver interface
+			if (strpos($return, "function slowQuery(")) {
+				unset($functions["connection_id"]); // slow_query() kills the query by its connection ID only if the driver can't limit the execution time itself
+			}
 			foreach ($functions as $val) {
 				if (!strpos($return, "$val(")) {
 					fprintf(STDERR, "Missing $val in $vendor\n");
